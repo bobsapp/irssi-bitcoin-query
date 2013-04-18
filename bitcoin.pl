9c0d34fd905f6f9eb0bc9b761e15500ec40dc36b
Arnold Rimmer <anonymous.of.london.south@gmail.com>
2013-04-18 23:20:02 +0100

    * Query MtGox API

--- bitcoin.pl
+++ bitcoin.pl
@@ -2,6 +2,9 @@ use strict;
 use vars qw($VERSION %IRSSI);
 
 use Irssi;
+use JSON;
+use LWP::Simple;
+
 $VERSION = '1.00';
 %IRSSI = (
     authors     => 'Arnold Judas Rimmer',
@@ -16,18 +19,25 @@ $VERSION = '1.00';
 sub btc {
     my ($data, $server, $witem) = @_;
     return unless $witem;
-    # $witem (window item) may be undef.
+    my $content = get("https://data.mtgox.com/api/2/BTCUSD/money/ticker");
+    my $response = $json->decode( $content );
 
-    $witem->print('It works!');
-    Irssi::active_win->command("/say sup dawg dat btc be blingin");
+    # $witem->print('It works!');
+    if( $response->{result} eq 'success' ) {
+    	my($high,$low,$avg) = map { $response->{data}->{$_}->{value} } qw(high low average);
+    	Irssi::active_win->command("/say BTCUSD: high:$high low:$low average:$avg");
+    	return;
+    }
+    Irssi::active_win->command("/say Failed to get mtGox data");
 }
 
 sub ltc {
     my ($data, $server, $witem) = @_;
     return unless $witem;
-    # $witem (window item) may be undef.
 
-    $witem->print('It works!');
+    # https://btc-e.com/exchange/ltc_usd
+
+    # $witem->print('It works!');
     Irssi::active_win->command("/say sup dawg dat ltc be blingin");
 }
 
